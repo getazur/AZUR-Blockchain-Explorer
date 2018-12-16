cbe4d968ed9c36d69528a3c2773867a491dc9a1a
getazur <45802475+getazur@users.noreply.github.com>
2018-12-16 02:45:06 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-var api = 'https://api.getazur.org';
+var api = '104.198.58.53:15251';
 var apiList = [""];
 var blockTargetInterval = 7;
 var coinUnits = 100;
 var symbol = 'AZUR';
-var refreshDelay = 5000;
+var refreshDelay = 1000;
 // pools stats by MainCoins
 var networkStat = {
     "azur": [
