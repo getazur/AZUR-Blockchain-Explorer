e262f0a433de725eec1cf122d95dbaee397899ee
Azur Network <45802475+getazur@users.noreply.github.com>
2019-01-20 04:39:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,14 @@ var apiList = [""];
 var blockTargetInterval = 7;
 var coinUnits = 100;
 var symbol = 'AZUR';
-var refreshDelay = 1000;
+var refreshDelay = 5000;
 // pools stats by MainCoins
 var networkStat = {
     "azur": [
-    ]
+["azur.thepiratemine.nl", "https://azur.thepiratemine.nl:2893/stats"],
+["azur.cryptomine.cx", "https://azur.cryptomine.cx:8009/stats"],
+["enigma-mining.de", "https://pool.enigma-mining.de:8015/stats"],
+["azur.cryptocoins-digging.space", "http://azur.cryptocoins-digging.space:8008/stats"]
+
+            ]
 };
